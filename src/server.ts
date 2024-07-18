d2dda245cee7ed6d73cd3021db0383566f927e69
titi-lima <titisau@gmail.com>
2024-07-18 12:16:07 -0300

    idk

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,5 @@ import "dotenv/config";
 import "./database";
 
 app.listen(process.env.SERVER_PORT || 3001, () => {
-  console.log(`Server is running 🚀 ${process.env.SERVER_PORT}`);
+  console.log(`Server is running 🚀 ${process.env.SERVER_PORT || 3001}`);
 });
